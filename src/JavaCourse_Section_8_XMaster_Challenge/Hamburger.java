d9eb4ba44c2c85abee5fd794ba08c3bdc311e5e2
Michał Kwaśny <kwas05@hotmail.com>
2018-09-24 16:53:22 +0200

    Commit#3

--- src/JavaCourse_Section_8_XMaster_Challenge/Hamburger.java
+++ src/JavaCourse_Section_8_XMaster_Challenge/Hamburger.java
@@ -44,7 +44,24 @@ public class Hamburger {
 
     public double itemizeHamburger(){
         double hamburgerPrice = this.price;
-        return 0; // do zmiany
+        System.out.println(this.name +" hamburger " + this.meat + " on a "+this.breadRollType + " roll" + " price is " + this.price);
+        if(this.addition1Name != null){
+            hamburgerPrice += this.addition1Price;
+            System.out.println("Added " +this.addition1Name+ " for an extra "+this.addition1Price);
+        }
+        if(this.addition2Name != null){
+            hamburgerPrice += this.addition2Price;
+            System.out.println("Added " +this.addition2Name+ " for an extra "+this.addition2Price);
+        }
+        if(this.addition3Name != null){
+            hamburgerPrice += this.addition3Price;
+            System.out.println("Added " +this.addition3Name+ " for an extra "+this.addition3Price);
+        }
+        if(this.addition4Name != null){
+            hamburgerPrice += this.addition4Price;
+            System.out.println("Added " +this.addition4Name+ " for an extra "+this.addition4Price);
+        }
+        return hamburgerPrice;
     }
 
 }
