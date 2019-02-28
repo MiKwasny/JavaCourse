d953a56f6a6991a5ce7a662361334b7ee1a4e747
Michał Kwaśny <kwas05@hotmail.com>
2019-02-28 17:44:42 +0100

    12

--- src/JavaCourse_Section_12_CollectionsListMethods/Theatre.java
+++ src/JavaCourse_Section_12_CollectionsListMethods/Theatre.java
@@ -23,25 +23,15 @@ public class Theatre {
     }
 
     public boolean reserveSeat(String seatNumber) {
-        int low = 0;
-        int high = seats.size()-1;
-
-        while (low <= high) {
-            System.out.print(".");
-            int mid = (low + high) / 2;
-            Seat midVal = seats.get(mid);
-            int cmp = midVal.getSeatNumber().compareTo(seatNumber);
-
-            if (cmp < 0) {
-                low = mid + 1;
-            } else if (cmp > 0) {
-                high = mid - 1;
-            } else {
-                return seats.get(mid).reserve();
-            }
+        Seat requestedSeat = new Seat(seatNumber);
+        // TODO binary search, LEARN IT!
+        int foundSeat = Collections.binarySearch(seats, requestedSeat, null);
+        if(foundSeat>=0){
+            return seats.get(foundSeat).reserve();
+        }else{
+            System.out.println("There is not seat "+seatNumber);
+            return false;
         }
-        System.out.println("There is no seat " + seatNumber);
-        return false;
     }
 
     // for testing
