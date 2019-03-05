0e4c6ffb4ff4dd9d3993d06181356b51acd176f1
Michał Kwaśny <kwas05@hotmail.com>
2019-03-05 18:17:59 +0100

    gg

--- src/JavaCourse_Section_12_Maps/MapClass.java
+++ src/JavaCourse_Section_12_Maps/MapClass.java
@@ -5,8 +5,48 @@ import java.util.Map;
 
 public class MapClass {
     public static void main(String[] args) {
+
         Map<String, String> map = new HashMap<>();
-        map.put("Java", "A compiled high level language");
+        if(map.containsKey("Java")){
+            System.out.println("java here");
+        }else{
+            map.put("Java", "A compiled high level language");
+            System.out.println("java added");
+        }
+
         map.put("Python", "something more");
+        map.put("Algol", "algoritm language");
+        map.put("Basic", "for begginers");
+        System.out.println(map.put("Lisp", "madnes!"));
+
+        //If it was put before it will be visible now as it was before
+        System.out.println(map.put("Java", "A compiled high level language"));
+
+        map.get("Lisp");
+
+        System.out.println(map.get("Lisp"));
+
+        map.put("Lisp", "lolex in this place");
+
+        System.out.println(map.get("Lisp"));
+
+        if(map.containsKey("Java")){
+            System.out.println("java here!");
+        }else{
+            System.out.println("this is not about java");
+        }
+
+        System.out.println("===========================================================");
+
+        for(String key:map.keySet()){
+            System.out.println(key +" "+map.get(key));
+
+        }
+
+        if(map.remove("Python", "something better")){
+            System.out.println("Python removed");
+        }else{
+            System.out.println("Python value/pair not found");
+        }
     }
 }
